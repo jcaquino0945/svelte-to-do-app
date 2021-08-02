6d6e0b7393a3286c818bfb8fb06075f3ba43db2d
Miguel Aquino <jcaquino0945@gmail.com>
2021-08-02 23:01:13 +0800

    added navbar and section 1

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+
+</script>
+
+<nav>
+    <div class="menu-left">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
+          </svg>
+    </div>
+    <div class="menu-right">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+    </div>
+  </nav>
+
+<style lang="postcss">
+    nav {
+    @apply px-4 my-2 flex absolute w-full
+  }
+  .menu-left {
+      @apply w-1/2 flex justify-start
+    }
+  .menu-right {
+      @apply w-1/2 flex justify-end
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+  import { theme } from './store'
+  import ToggleTheme from '$lib/ToggleTheme.svelte'
+  import Counter from '$lib/Counter.svelte'
+  import Timer from '$lib/Timer.svelte'
+  import Navbar from '../lib/Navbar.svelte'
+</script>
+
+<svelte:head>
+  <!-- Primary Meta Tags -->
+  <title>SvelteKit ❤️ Tailwind 2</title>
+  <meta name="title" content="SvelteKit ❤️ Tailwind 2" />
+  <meta name="description" content="SvelteKit and Tailwind 2 toggle dark/light theme demo" />
+  <meta name="keywords" content="Svelte, SvelteKit, Svelte-kit, Tailwind, HTML, CSS, JavaScript" />
+  <meta name="author" content="@dansvel" />
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://metatags.io/" />
+  <meta property="og:title" content="SvelteKit ❤️ Tailwind 2" />
+  <meta property="og:description" content="SvelteKit and Tailwind 2 toggle dark/light theme demo" />
+  <meta
+    property="og:image"
+    content="https://sveltekit-tailwind2.netlify.app/screenshot.png"
+  />
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:url" content="https://metatags.io/" />
+  <meta property="twitter:title" content="SvelteKit ❤️ Tailwind 2" />
+  <meta
+    property="twitter:description"
+    content="SvelteKit and Tailwind 2 toggle dark/light theme demo"
+  />
+  <meta
+    property="twitter:image"
+    content="https://sveltekit-tailwind2.netlify.app/screenshot.png"
+  />
+</svelte:head>
+
+<!-- write html code here-->
+<Navbar></Navbar>
+
+<main class="h-auto w-full">
+  <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 z-50 right-4 bottom-5 text-white fixed bg-blue-500 rounded-full bg-clip-content bg-cover add-icon opacity-70" viewBox="0 0 20 20" fill="currentColor">
+    <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd" />
+  </svg>
+<div class="section-1">
+<div class="section-1-overlay">
+    <div class="container mx-auto h-full flex justify-end px-4 section-1-body flex-col">
+        <h1 class="text-4xl text-white font-thin mb-2">What Will You Do Today?</h1>
+        <p class="text-xl text-white font-thin mb-2">October 26 2020</p>
+    </div>
+</div>
+</div>
+</main>
+
+
+<style lang="postcss">
+.section-1 {
+    background-image: url('../../static/80622072_818311235304313_8533843013643087754_n.jpg/');
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 40vh;
+    background-color: red;
+    width: 100%;
+}
+.section-1-overlay {
+    background-color: #0568c5ad;
+    width: 100%;
+    height: 100%;
+}
+.section-1-body h1 {
+    font-family: 'Antonio', sans-serif;
+}
+.section-1-body p {
+    font-family: 'Abel', sans-serif;
+}
+</style>
